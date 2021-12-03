6af3ab35a874331607e93ae1f57e51c9e079c39e
Laia <laia.fustercorral@gmail.com>
2021-12-03 19:07:01 +0100

    Added player controller that lets you walk, jump and deal with slopes

---
+++ PEC3_UnJuegoDeArtilleria/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,111 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed = 5;
+    [SerializeField] float floorCheckDistance = 0.1f;
+    [SerializeField] LayerMask groundMask;
+    [SerializeField] Transform feet;
+    [SerializeField] PhysicsMaterial2D fullFriction;
+    [SerializeField] PhysicsMaterial2D noFriction;
+    [SerializeField] float jumpForce = 10;
+
+    private Rigidbody2D rigidbody2d;
+    private bool isGrounded;
+    private float slopeAngle;
+    private Vector2 slopeNormalPerpendicular;
+    private bool isOnSlope;
+    private float previousSlopeAngle;
+    private RaycastHit2D hit;
+    private bool isJumping;
+    private bool canJump;
+    private float horizontalInput;
+
+    private void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        CheckIsGrounded();
+
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        GetSlopeInfo();
+
+        Move();
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+
+    private void GetSlopeInfo()
+    {
+        hit = Physics2D.Raycast(feet.position, Vector2.down, floorCheckDistance, groundMask);
+        if (hit)
+        {
+            isGrounded = true;
+            slopeNormalPerpendicular = Vector2.Perpendicular(hit.normal).normalized;
+            slopeAngle = Vector2.Angle(hit.normal, Vector2.up);
+
+            if (slopeAngle != previousSlopeAngle)
+            {
+                isOnSlope = true;
+                previousSlopeAngle = slopeAngle;
+            }
+        }
+
+        if (isOnSlope && horizontalInput == 0)
+        {
+            rigidbody2d.sharedMaterial = fullFriction;
+        }
+        else
+        {
+            rigidbody2d.sharedMaterial = noFriction;
+        }
+    }
+
+    private void CheckIsGrounded()
+    {
+        isGrounded = Physics2D.OverlapCircle(feet.position, floorCheckDistance, groundMask);
+
+        if(rigidbody2d.velocity.y <= 0f)
+        {
+            isJumping = false;
+        }
+
+        if(isGrounded && !isJumping)
+        {
+            canJump = true;
+        }    
+    }
+
+    private void Jump()
+    {
+        if(canJump)
+        {
+            canJump = false;
+            isJumping = true;
+            rigidbody2d.AddForce(new Vector3(0, jumpForce, 0), ForceMode2D.Impulse);
+        }
+    }
+
+    private void Move()
+    {
+        if (isGrounded && !isOnSlope && !isJumping)
+        {
+            rigidbody2d.velocity = new Vector3(speed * horizontalInput, 0, 0);
+        }
+        else if (isGrounded && isOnSlope && !isJumping)
+        {
+            rigidbody2d.velocity = new Vector3(speed * slopeNormalPerpendicular.x * -horizontalInput, speed * slopeNormalPerpendicular.y * -horizontalInput, 0);
+        }
+        else if (!isGrounded)
+        {
+            rigidbody2d.velocity = new Vector3(speed * horizontalInput, rigidbody2d.velocity.y);
+        }
+    }
+}
